958c0a92f0300db23aee2a796043ebb3e18b13cd
Yassar Hammami <yassarhammami@gmail.com>
2022-07-22 02:17:43 +0300

    Update MonitorApplication.java

--- src/main/java/com/jazara/icu/monitor/MonitorApplication.java
+++ src/main/java/com/jazara/icu/monitor/MonitorApplication.java
@@ -30,36 +30,48 @@ public class MonitorApplication {
         SpringApplication.run(MonitorApplication.class, args);
     }
 
-    @Configuration(proxyBeanMethods = false)
-    public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
-
+    @Configuration
+    public static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         private final AdminServerProperties adminServer;
 
-        public SecuritySecureConfig(AdminServerProperties adminServer) {
+        public WebSecurityConfig(AdminServerProperties adminServer) {
             this.adminServer = adminServer;
         }
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+            SavedRequestAwareAuthenticationSuccessHandler successHandler =
+                    new SavedRequestAwareAuthenticationSuccessHandler();
             successHandler.setTargetUrlParameter("redirectTo");
-            successHandler.setDefaultTargetUrl(this.adminServer.path("/"));
+            successHandler.setDefaultTargetUrl(this.adminServer.getContextPath() + "/");
 
-            http.authorizeRequests(
-                    (authorizeRequests) -> authorizeRequests.antMatchers(this.adminServer.path("/assets/**")).permitAll()
-                            .antMatchers(this.adminServer.path("/login")).permitAll().anyRequest().authenticated()
-            ).formLogin(
-                    (formLogin) -> formLogin.loginPage(this.adminServer.path("/login")).successHandler(successHandler).and()
-            ).logout((logout) -> logout.logoutUrl(this.adminServer.path("/logout"))).httpBasic(Customizer.withDefaults())
-                    .csrf((csrf) -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                            .ignoringRequestMatchers(
-                                    new AntPathRequestMatcher(this.adminServer.path("/instances"),
-                                            HttpMethod.POST.toString()),
-                                    new AntPathRequestMatcher(this.adminServer.path("/instances/*"),
-                                            HttpMethod.DELETE.toString()),
-                                    new AntPathRequestMatcher(this.adminServer.path("/actuator/**"))
-                            ))
-                    .rememberMe((rememberMe) -> rememberMe.key(UUID.randomUUID().toString()).tokenValiditySeconds(1209600));
+            http
+                    .authorizeRequests()
+                    .antMatchers(this.adminServer.getContextPath() + "/assets/**").permitAll()
+                    .antMatchers(this.adminServer.getContextPath() + "/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin()
+                    .loginPage(this.adminServer.getContextPath() + "/login")
+                    .successHandler(successHandler)
+                    .and()
+                    .logout()
+                    .logoutUrl(this.adminServer.getContextPath() + "/logout")
+                    .and()
+                    .httpBasic()
+                    .and()
+                    .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .ignoringRequestMatchers(
+                            new AntPathRequestMatcher(this.adminServer.getContextPath() +
+                                    "/instances", HttpMethod.POST.toString()),
+                            new AntPathRequestMatcher(this.adminServer.getContextPath() +
+                                    "/instances/*", HttpMethod.DELETE.toString()),
+                            new AntPathRequestMatcher(this.adminServer.getContextPath() + "/actuator/**"))
+                    .and()
+                    .rememberMe()
+                    .key(UUID.randomUUID().toString())
+                    .tokenValiditySeconds(1209600);
         }
 
         // Required to provide UserDetailsService for "remember functionality"
