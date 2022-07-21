7351bdf3bc61efd116888e4e0a98dd076f9c3c8a
Yassar Hammami <yassarhammami@gmail.com>
2022-07-22 02:10:23 +0300

    .

--- src/main/java/com/jazara/icu/monitor/MonitorApplication.java
+++ src/main/java/com/jazara/icu/monitor/MonitorApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
@@ -28,48 +30,43 @@ public class MonitorApplication {
         SpringApplication.run(MonitorApplication.class, args);
     }
 
-    @Configuration
-    public static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Configuration(proxyBeanMethods = false)
+    public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+
         private final AdminServerProperties adminServer;
 
-        public WebSecurityConfig(AdminServerProperties adminServer) {
+        public SecuritySecureConfig(AdminServerProperties adminServer) {
             this.adminServer = adminServer;
         }
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            SavedRequestAwareAuthenticationSuccessHandler successHandler =
-                    new SavedRequestAwareAuthenticationSuccessHandler();
+            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
             successHandler.setTargetUrlParameter("redirectTo");
-            successHandler.setDefaultTargetUrl(this.adminServer.getContextPath() + "/");
+            successHandler.setDefaultTargetUrl(this.adminServer.path("/"));
 
-            http
-                    .authorizeRequests()
-                    .antMatchers(this.adminServer.getContextPath() + "/assets/**").permitAll()
-                    .antMatchers(this.adminServer.getContextPath() + "/login").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
-                    .formLogin()
-                    .loginPage(this.adminServer.getContextPath() + "/login")
-                    .successHandler(successHandler)
-                    .and()
-                    .logout()
-                    .logoutUrl(this.adminServer.getContextPath() + "/logout")
-                    .and()
-                    .httpBasic()
-                    .and()
-                    .csrf()
-                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                    .ignoringRequestMatchers(
-                            new AntPathRequestMatcher(this.adminServer.getContextPath() +
-                                    "/instances", HttpMethod.POST.toString()),
-                            new AntPathRequestMatcher(this.adminServer.getContextPath() +
-                                    "/instances/*", HttpMethod.DELETE.toString()),
-                            new AntPathRequestMatcher(this.adminServer.getContextPath() + "/actuator/**"))
-                    .and()
-                    .rememberMe()
-                    .key(UUID.randomUUID().toString())
-                    .tokenValiditySeconds(1209600);
+            http.authorizeRequests(
+                    (authorizeRequests) -> authorizeRequests.antMatchers(this.adminServer.path("/assets/**")).permitAll()
+                            .antMatchers(this.adminServer.path("/login")).permitAll().anyRequest().authenticated()
+            ).formLogin(
+                    (formLogin) -> formLogin.loginPage(this.adminServer.path("/login")).successHandler(successHandler).and()
+            ).logout((logout) -> logout.logoutUrl(this.adminServer.path("/logout"))).httpBasic(Customizer.withDefaults())
+                    .csrf((csrf) -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                            .ignoringRequestMatchers(
+                                    new AntPathRequestMatcher(this.adminServer.path("/instances"),
+                                            HttpMethod.POST.toString()),
+                                    new AntPathRequestMatcher(this.adminServer.path("/instances/*"),
+                                            HttpMethod.DELETE.toString()),
+                                    new AntPathRequestMatcher(this.adminServer.path("/actuator/**"))
+                            ))
+                    .rememberMe((rememberMe) -> rememberMe.key(UUID.randomUUID().toString()).tokenValiditySeconds(1209600));
         }
+
+        // Required to provide UserDetailsService for "remember functionality"
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication().withUser("user").password("{noop}password").roles("USER");
+        }
+
     }
 }
