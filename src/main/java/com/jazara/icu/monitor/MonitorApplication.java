4d68011c57b90fc1dcde733e09285a6b389af26e
Yassar Hammami <yassarhammami@gmail.com>
2022-07-22 18:30:47 +0300

    .

--- src/main/java/com/jazara/icu/monitor/MonitorApplication.java
+++ src/main/java/com/jazara/icu/monitor/MonitorApplication.java
@@ -4,13 +4,11 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableAdminServer
-@EnableTurbineStream
 @EnableEncryptableProperties
 public class MonitorApplication {
 
